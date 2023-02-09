5d714a796c117bb59d45c7e5e56ae0c9ab6d07f5
mohamed-hassan93 <mohamed.hassan93@outlook.com>
2023-02-08 22:27:35 -0600

    added wanted posters to One Piece API

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 2330
+
+
+app.use(cors());
 
 const crewmates = {
 
@@ -11,7 +15,8 @@ const crewmates = {
         'Devil Fruit User': 'Yes',
         'Devil Fruit': 'Gomu Gomu No Mi',
         'Haki': 'Arnmament, Observation, Conqueror',
-        'Bounty': 3000000000
+        'Bounty': 3000000000,
+        'Wanted Poster': 'https://static.wikia.nocookie.net/onepiece/images/1/17/Monkey_D._Luffy%27s_Seventh_Wanted_Poster.png/revision/latest?cb=20220620183756'
     },
 
     'zoro': {
@@ -20,7 +25,8 @@ const crewmates = {
         'Devil Fruit User': 'No',
         'Devil Fruit': 'N/Ap',
         'Haki': 'Arnmament, Observation, Conqueror',
-        'Bounty': 1111000000
+        'Bounty': 1111000000,
+        'Wanted Poster' : 'https://static.wikia.nocookie.net/onepiece/images/5/5d/Roronoa_Zoro%27s_Current_Wanted_Poster.png/revision/latest?cb=20161222000618'
     },
 
     'nami': {
@@ -29,7 +35,8 @@ const crewmates = {
         'Devil Fruit User': 'No',
         'Devil Fruit': 'N/Ap',
         'Haki': 'Unconfirmed',
-        'Bounty': 366000000
+        'Bounty': 366000000,
+        'Wanted Poster' : 'https://static.wikia.nocookie.net/onepiece/images/3/3b/Nami%27s_Current_Wanted_Poster.png/revision/latest?cb=20160619204508'
     },
 
     'usopp': {
@@ -38,7 +45,8 @@ const crewmates = {
         'Devil Fruit User': 'No',
         'Devil Fruit': 'N/Ap',
         'Haki': 'Observation',
-        'Bounty': 500000000
+        'Bounty': 500000000,
+        'Wanted Poster': 'https://static.wikia.nocookie.net/onepiece/images/5/5e/God_Usopp%27s_Wanted_Poster.png/revision/latest?cb=20160619204644'
     },
 
     'sanji': {
@@ -47,7 +55,8 @@ const crewmates = {
         'Devil Fruit User': 'No',
         'Devil Fruit': 'N/Ap',
         'Haki': 'Armament, Observation',
-        'Bounty': 1032000000
+        'Bounty': 1032000000,
+        'Wanted Poster': 'https://static.wikia.nocookie.net/onepiece/images/c/ca/Sanji%27s_Current_Wanted_Poster.png/revision/latest?cb=20190331050119'
     },
 
     'chopper': {
@@ -56,7 +65,8 @@ const crewmates = {
         'Devil Fruit User': 'Yes',
         'Devil Fruit': 'Hito Hito No Mi',
         'Haki': 'Unconfirmed',
-        'Bounty': 1000
+        'Bounty': 1000,
+        'Wanted Poster': 'https://static.wikia.nocookie.net/onepiece/images/7/75/Tony_Tony_Chopper%27s_Current_Wanted_Poster.png/revision/latest?cb=20160619204650'
     },
 
 
@@ -66,7 +76,8 @@ const crewmates = {
         'Devil Fruit User': 'Yes',
         'Devil Fruit': 'Hana Hana No Mi',
         'Haki': 'Unconfirmed',
-        'Bounty': 930000000
+        'Bounty': 930000000,
+        'Wanted Poster' : 'https://static.wikia.nocookie.net/onepiece/images/b/b8/Nico_Robin%27s_Current_Wanted_Poster.png/revision/latest?cb=20161222000656'
     },
 
     'franky': {
@@ -75,7 +86,8 @@ const crewmates = {
         'Devil Fruit User': 'No',
         'Devil Fruit': 'N/Ap',
         'Haki': 'Unconfirmed',
-        'Bounty': 394000000
+        'Bounty': 394000000,
+        'Wanted Poster': 'https://static.wikia.nocookie.net/onepiece/images/1/1d/Franky%27s_Wanted_Poster.png/revision/latest?cb=20130624084046'
     },
 
     'brook': {
@@ -84,7 +96,8 @@ const crewmates = {
         'Devil Fruit User': 'Yes',
         'Devil Fruit': 'Yomi Yomi No Mi',
         'Haki': 'Unconfirmed',
-        'Bounty': 383000000
+        'Bounty': 383000000,
+        'Wanted Poster': 'https://static.wikia.nocookie.net/onepiece/images/0/09/Brook%27s_Concert_Wanted_Poster.png/revision/latest?cb=20160619204634'
     },
 
     'jinbe': {
@@ -93,7 +106,8 @@ const crewmates = {
         'Devil Fruit User': 'No',
         'Devil Fruit': 'N/Ap',
         'Haki': 'Observation, Armament',
-        'Bounty': 1100000000
+        'Bounty': 1100000000,
+        'Wanted Poster': 'https://static.wikia.nocookie.net/onepiece/images/7/71/Jinbe%27s_Current_Wanted_Poster.png/revision/latest?cb=20211121074902'
     },
 
     'buggy the clown - you goofed': {
@@ -102,7 +116,8 @@ const crewmates = {
         'Devil Fruit User': 'Yes',
         'Devil Fruit': 'Bara Bara No Mi',
         'Haki': 'Unconfirmed',
-        'Bounty': 3189000000
+        'Bounty': 3189000000,
+        'Wanted Poster': 'https://static.wikia.nocookie.net/onepiece/images/2/22/Buggy%27s_Wanted_Poster.png/revision/latest?cb=20130215232715'
     },
 
 }
